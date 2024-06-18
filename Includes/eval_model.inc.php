9a06f6626368360308ecd114cabb0364df014e8c
githinjialbert <githinjialbert001@gmail.com>
2024-06-18 16:51:55 +0300

    some code and name change

---
+++ Includes/eval_model.inc.php
@@ -0,0 +1,42 @@
+<?php 
+
+class EvaluationModel {
+
+private $dbserver = 'localhost';
+private $dbname = 'evaluation_form';
+private $dbusername = 'root';
+private $dbpassword = '';
+private $conn;
+
+public function __construct() {
+
+    try {
+
+        $conn = new PDO("mysql:host=$dbserver; dbname=$dbname", $dbusername, $dbpassword);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
+    } catch (PDOException $e) {
+        die("Connection failed: " . $e->getMessage());
+    }
+}
+
+public function insertEvaluation($fname, $lname, $age, $impression, $rating, $improvement) {
+    try {
+
+        $stmt = $this->conn->prepare("INSERT INTO evaluations (fname, lname, age, impression, rating, improvement) VALUES(:fname, :lname, :age, :impression, :rating, :improvement);");
+        
+        $stmt->bindParam(":fname", $fname);
+        $stmt->bindParam(":lname", $lname);
+        $stmt->bindParam(":age", $age);
+        $stmt->bindParam(":impression", $impression);
+        $stmt->bindParam(":rating", $rating);
+        $stmt->bindParam(":improvement", $improvement);
+
+        $stmt->execute();
+    } catch (PDOException $e) {
+        die("Error inserting evaluation: " . $e->getMessage());
+    }
+}
+
+}
+
